64fac8d081401a69be48d9d30ac075222edb967f
ehsan096 <53744892+ehsan096@users.noreply.github.com>
2021-12-17 20:40:55 +0500

    update

--- src/component/LogoCategory/Logocategory.js
+++ src/component/LogoCategory/Logocategory.js
@@ -19,44 +19,62 @@ import business from "../../images/business.png";
 import sport from "../../images/sport.png";
 import letter from "../../images/letter.png";
 import medical from "../../images/medical.png";
+import axios from "axios";
+import categoryService from "../../services/Categories";
 
 const Logocategory = () => {
   const classes = useStyle();
   let variable = "";
-  let categories = [
-    {
-      name: "Education",
-      image: eduction,
-    },
-    {
-      name: "Fashion",
-      image: beauty,
-    },
-    {
-      name: "Animal",
-      image: animal,
-    },
-    {
-      name: "Food",
-      image: food,
-    },
-    {
-      name: "Business",
-      image: business,
-    },
-    {
-      name: "Sports",
-      image: sport,
-    },
-    {
-      name: "Medical",
-      image: medical,
-    },
-    {
-      name: "Letter",
-      image: letter,
-    },
-  ];
+  const [categories, setCategories] = React.useState([]);
+  const getCategories = () => {
+    categoryService
+      .getCategories()
+      .then((data) => {
+        console.log("Get main Logo Data > ", data);
+        setCategories(data.categories);
+      })
+      .catch((err) => {
+        console.log("Error in Main Logo", err);
+      });
+  };
+  React.useEffect(() => {
+    getCategories();
+  }, []);
+
+  // let categories = [
+  //   {
+  //     name: "Education",
+  //     image: eduction,
+  //   },
+  //   {
+  //     name: "Fashion",
+  //     image: beauty,
+  //   },
+  //   {
+  //     name: "Animal",
+  //     image: animal,
+  //   },
+  //   {
+  //     name: "Food",
+  //     image: food,
+  //   },
+  //   {
+  //     name: "Business",
+  //     image: business,
+  //   },
+  //   {
+  //     name: "Sports",
+  //     image: sport,
+  //   },
+  //   {
+  //     name: "Medical",
+  //     image: medical,
+  //   },
+  //   {
+  //     name: "Letter",
+  //     image: letter,
+  //   },
+  // ];
 
   return (
     <div>
@@ -65,33 +83,37 @@ const Logocategory = () => {
           Discover 1000+ Professional Grade Logo Designs
         </Typography>
         <Grid container spacing={3}>
-          {categories.map((category) => {
-            console.log("categories.map > ", category);
-            variable = category.name;
-            return (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={category.name}>
-                <Link
-                  to={`/selectlogo/${variable}`}
-                  className={classes.cardlink}
-                >
-                  <Card className={classes.grid}>
-                    <CardActionArea>
-                      <CardMedia
-                        image={category.image}
-                        className={classes.media}
-                      />
-                      <Divider />
-                      <CardContent>
-                        <Typography varient="h6" component="h2">
-                          {category.name} Logo
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Link>
-              </Grid>
-            );
-          })}
+          {categories.length > 1
+            ? categories.map((category) => {
+                console.log("categories.map > ", category);
+                variable = category.logoName;
+                return (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={category.name}>
+                    <Link
+                      to={`/selectlogo/${category.logoName}`}
+                      className={classes.cardlink}
+                    >
+                      <Card className={classes.grid}>
+                        <CardActionArea>
+                          <CardMedia
+                            image={`data:image/svg+xml;base64,${btoa(
+                              unescape(encodeURIComponent(category.logoSvg))
+                            )}`}
+                            className={classes.media}
+                          />
+                          <Divider />
+                          <CardContent>
+                            <Typography varient="h6" component="h2">
+                              {category.logoName} Logo
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Link>
+                  </Grid>
+                );
+              })
+            : ""}
         </Grid>
       </Container>
     </div>
